3e0f0a932fe2081b94548e39cecbacba747ad67e
hth225 <hth225@naver.com>
2018-08-22 19:08:43 +0900

    Check out some APIs

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <ActivityIndicator />
         <Text>Hello! This is React Native! And i love it!</Text>
       </View>
     );
