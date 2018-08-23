015b22f186431ecf312774178028f167a2bd8264
hth225 <hth225@naver.com>
2018-08-23 13:32:06 +0900

    Error handling

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,34 +1,56 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import Weather from "./Weather";
+import { LinearGradient } from "expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error: null
   };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({
-          isLoaded: true
+          isLoaded: true,
         });
       },
       error => {
-        console.log(error);
+        this.setState({
+          error: error
+        });
       }
     );
   }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
         {isLoaded ? (
           <Weather />
         ) : (
-          <View style={styles.loading}>
-            <Text style={styles.loadingText}>날씨 불러오는 중</Text>
-          </View>
+          <LinearGradient
+            colors={["#a18cd1", "#fbc2eb"]}
+            style={styles.loading}
+          >
+            <View style={styles.loading}>
+              <Text style={styles.loadingText}>
+                Getting Current Location...
+              </Text>
+              {error ? (
+                <View >
+                  <MaterialCommunityIcons
+                    color="red"
+                    size={144}
+                    name="account-alert"
+                  />
+                  <Text style={styles.errorText}>{error}</Text>
+                </View>
+              ) : null}
+            </View>
+          </LinearGradient>
         )}
       </View>
     );
@@ -41,14 +63,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
     // Default value of flexDirection is 'column'
   },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    fontSize: 25,
+    fontWeight: "800"
+  },
   loading: {
     flex: 1,
-    backgroundColor: "white",
-    justifyContent: "flex-start",
-    paddingLeft: 24
+    justifyContent: "center",
+    paddingLeft: 20
   },
   loadingText: {
-    fontSize: 30,
-    paddingTop: 120
+    fontSize: 25,
+    paddingBottom: 70,
+    color: "white",
+    fontWeight: "700"
   }
 });
