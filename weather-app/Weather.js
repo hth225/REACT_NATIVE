8eec0cc6aa7e5319a8b80c0ff1151e5c9d8e8c78
hth225 <hth225@naver.com>
2018-09-07 17:43:32 +0900

    some changes

--- weather-app/Weather.js
+++ weather-app/Weather.js
@@ -55,57 +55,6 @@ const weatherCases = {
   }
 };
 
-// const weatherCases = {
-//   Rain: {
-//     colors: ["#283E51", "#4B79A1"],
-//     title: "우산 챙겨가 😘",
-//     subtitle: "비맞으면 감기 걸려... 😂",
-//     icon: "weather-pouring"
-//   },
-//   Clear: {
-//     colors: ["#FDC830", "#F37335"],
-//     title: "날씨 좋다!! 너랑 놀러가고 싶어",
-//     subtitle: "조금 더울지도 모르겠지만 ! 🤣",
-//     icon: "weather-sunny"
-//   },
-//   Thunderstorm: {
-//     colors: ["#2c3e50", "#bdc3c7"],
-//     title: "천둥번개🌩 친대!",
-//     subtitle: "무서운거 아니까 빨리 나한테 전화해줘",
-//     icon: "weather-lightning"
-//   },
-//   Clouds: {
-//     colors: ["#4B79A1", "#283E51"],
-//     title: "구름☁️ 많음!",
-//     subtitle: "우울한 날이네 너가 보고싶어..",
-//     icon: "weather-cloudy"
-//   },
-//   Snow: {
-//     colors: ["#E6DADA", "#274046"],
-//     title: "눈 온대!!! ☃️",
-//     subtitle: "진경이의 휴가철이 찾아왔습니다! 예!❄️",
-//     icon: "weather-snowy"
-//   },
-//   Drizzle: {
-//     colors: ["#4CA1AF", "#2C3E50"],
-//     title: "이슬비🌧",
-//     subtitle: "많이 안온다고 우산 안쓰고 그럼 안된다??",
-//     icon: "weather-rainy"
-//   },
-//   Haze: {
-//     colors: ["#3a6186", "#89253e"],
-//     title: "실안개",
-//     subtitle: "공기 안좋으니까 마스크 챙겨가😉",
-//     icon: "weather-fog"
-//   },
-//   Mist: {
-//     colors: ["#606c88", "#3f4c6b"],
-//     title: "안개 🌫",
-//     subtitle: "운전 조심해야 해 !!",
-//     icon: "weather-fog"
-//   }
-// };
-
 function Weather({ weatherName, temp }) {
   console.log(weatherName);
   return (
